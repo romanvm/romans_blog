a4f8ee8ddc94443eb899e0f800c94ccbfa530e2e
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-11-17 11:46:36 +0200

    Updates gitignore, refactors static urls for debug

--- romans_blog/urls.py
+++ romans_blog/urls.py
@@ -20,4 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
