68a04b62d44f4025420b07ec584791c6448e8020
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-11-17 11:21:41 +0200

    Changed default timezone, sets /static/ and /media/ paths

--- romans_blog/settings.py
+++ romans_blog/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -100,3 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
