235c39b325f3a4db51fb09ad6f2c3e1518496ed8
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-11-18 13:54:01 +0200

    Adds PageModelTestCase

--- pages/pages_tests/models_tests.py
+++ pages/pages_tests/models_tests.py
@@ -6,10 +6,17 @@
 from django.test import TestCase
 from ..models import Page
 
+page_content = """
+<h1>Lorem ipsum</h1>
+<p>Lorem ipsum dolor sit amet</p>
+"""
 
 class PageModelTestCase(TestCase):
     """
     Test for page model
     """
     def test_creating_page_objects(self):
-        self.fail('The test is not implemented!')
+        Page.objects.create(title='Page 2', content=page_content)
+        Page.objects.create(title='Page 1', content=page_content)
+        titles = [page.title for page in Page.objects.all()]
+        self.assertEqual(titles, ['Page 1', 'Page 2'])
