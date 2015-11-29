1f9dc832df6101aafa9ed550a45c11099e9f068f
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-11-29 13:20:05 +0200

    Adds blog views

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,10 @@
 # Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
 
 from django.conf.urls import url
+from .views import BlogHomeView, BlogPostView, BlogFeaturedView
 
 urlpatterns = [
-
+    url(r'^(?P<slug>[\w-]+)-(?P<pk>\d+)/$', BlogPostView.as_view(), name='blog_post'),
+    url(r'^featured/$', BlogFeaturedView.as_view(), name='featured_posts'),
+    url(r'^$', BlogHomeView.as_view(), name='blog_home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,54 @@
-import os
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.conf import settings
+from django.utils.translation import ugettext
+from .models import Post
 
+_ = ugettext
+
+
+class _PostsListView(ListView):
+    """
+    Base class for displaying post lists
+    """
+    template_name = '{0}/blog_posts_list.html'.format(settings.CURRENT_SKIN)
+    context_object_name = 'posts'
+    paginate_by = settings.BLOG_POSTS_PAGINATE_BY
+
+    def get_page_title(self):
+        """
+        Add page title to the posts list, e.g. 'Featured Posts'
+        """
+        return None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = self.get_page_title()
+        return context
+
+
+class BlogHomeView(_PostsListView):
+    """
+    Displays the list of all published posts starting from the recent.
+    """
+    queryset = Post.objects.filter(is_published=True)
+
+
+class BlogFeaturedView(_PostsListView):
+    """
+    Displays the list of featured posts
+    """
+    queryset = Post.objects.filter(is_published=True, is_featured=True)
+
+    def get_page_title(self):
+        return _('Featured Posts')
+
+
+class BlogPostView(DetailView):
+    """
+    Displays a blog post page
+    """
+    template_name = '{0}/blog_post.html'.format(settings.CURRENT_SKIN)
+    queryset = Post.objects.filter(is_published=True)
+    context_object_name = 'post'
+    query_pk_and_slug = True
