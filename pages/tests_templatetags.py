cbc6f59532c85d86ae3cc9fb92e9a2d0282fe64a
Roman Miroshnychenko <romanvm@yandex.ru>
2015-12-30 14:38:33 +0200

    Adds tests for pages views and templatetags

--- pages/tests_templatetags.py
+++ pages/tests_templatetags.py
@@ -3,23 +3,20 @@
 # Created on: 19.11.2015
 # Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
 
-import unittest
 from django.test import TestCase
 from .models import Page, MenuLink
+from .templatetags.pages_tags import get_pages_menu_links
 
 
 class GetMenuLinksTestCase(TestCase):
     """
     Test page view
     """
-    @unittest.expectedFailure
     def test_opening_page(self):
-        self.fail('Not implemented!')
         page = Page(title='Page', content='')
         page.save()
         MenuLink.objects.create(caption='Page 1', slug='page-1', page=page)
-        MenuLink.objects.create(caption='Page 2', slig='page-2', page=page)
-        response = self.client.get('/page-1/')
-        self.assertIn('menu_links', response.context)
-        self.assertContains(response, 'Page 1')
-        self.assertContains(response, 'Page 2')
+        MenuLink.objects.create(caption='Page 2', slug='page-2')
+        menu_links = get_pages_menu_links()
+        self.assertEqual(len(menu_links), 1)
+        self.assertEqual(menu_links[0].caption, 'Page 1')
--- pages/tests_views.py
+++ pages/tests_views.py
@@ -3,4 +3,21 @@
 # Created on: 19.11.2015
 # Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from .models import Page, MenuLink
+
+
+class PageViewTestCase(TestCase):
+    def test_opening_page_view(self):
+        page = Page(title='Lorem Ipsum', content='<b>Lorem ipsum dolor sit amet.</b>')
+        page.save()
+        link1 = MenuLink(caption='Page 1', slug='page-1', page=page)
+        link1.save()
+        link2 = MenuLink(caption='Page 2', slug='page-2')
+        link2.save()
+        response = self.client.get(reverse('pages:page', kwargs={'slug': 'page-1'}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Lorem ipsum dolor sit amet.')
+        response = self.client.get(reverse('pages:page', kwargs={'slug': 'page-2'}))
+        self.assertEqual(response.status_code, 404)
