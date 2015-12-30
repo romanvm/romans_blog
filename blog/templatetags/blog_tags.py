a7c3620bfb3bc904c7619331218f52a03e5df6b6
Roman Miroshnychenko <romanvm@yandex.ru>
2015-12-30 14:06:09 +0200

    Adds get_url template tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -76,31 +76,9 @@ def get_blog_menu_links():
     )
 
 
-@register.tag(name='captureas')
-def do_captureas(parser, token):
+@register.assignment_tag
+def get_url(url_name, *args, **kwargs):
     """
-    Allows to capture template tag output as a template variable.
-
-    Example::
-
-        {% captureas home_url %}{% url "blog:blog_home" %}{% endcaptureas %}
-        <li class="{% if request.path == home_url %}active{% endif %}">
+    Get reverse URL as a template variable
     """
-    try:
-        tag_name, args = token.contents.split(None, 1)
-    except ValueError:
-        raise template.TemplateSyntaxError("'captureas' node requires a variable name.")
-    nodelist = parser.parse(('endcaptureas',))
-    parser.delete_first_token()
-    return CaptureasNode(nodelist, args)
-
-
-class CaptureasNode(template.Node):
-    def __init__(self, nodelist, varname):
-        self.nodelist = nodelist
-        self.varname = varname
-
-    def render(self, context):
-        output = self.nodelist.render(context)
-        context[self.varname] = output
-        return ''
+    return reverse(url_name, args=args, kwargs=kwargs)
