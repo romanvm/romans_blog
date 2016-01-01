8bb33c698f9db6a0c475e0d173e1f26ac4b63f6a
Roman Miroshnychenko <romanvm@yandex.ru>
2016-01-01 17:33:05 +0200

    Removes forgotten debug code

--- blog/views.py
+++ blog/views.py
@@ -119,8 +119,3 @@ class BlogPostSearchView(SearchView):
 
     def get_queryset(self):
         return super().get_queryset().highlight()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        print(context)
-        return context
