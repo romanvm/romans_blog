8ff8324a5ec1acebf71d0fe72a677cc32e0babb0
Roman Miroshnychenko <romanvm@yandex.ru>
2016-01-01 17:32:41 +0200

    Adds custom html truncator filter

--- bootstrap_skin/templatetags/bootstrap_skin_tags.py
+++ bootstrap_skin/templatetags/bootstrap_skin_tags.py
@@ -5,6 +5,7 @@
 # E-mail: romanvm@yandex.ua
 
 from django import template
+from django.utils.text import Truncator
 
 register = template.Library()
 
@@ -18,3 +19,12 @@ def render_post_categories(post):
     for category in post.categories.all():
         categories.append('<a href="{url}">{name}</a>'.format(url=category.get_absolute_url(), name=category.name))
     return ',&nbsp;'.join(categories)
+
+
+@register.filter
+def truncate_post(post, words):
+    """
+    Truncate a Pots text to a given number of words.
+    """
+    terminator = '&nbsp;(<strong><a href="{0}">...</a></strong>)'.format(post.get_absolute_url())
+    return Truncator(post.content).words(words, truncate=terminator, html=True)
