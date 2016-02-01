c75bb640ba9de5c6788d2912321d4b18a84775bc
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2016-02-01 13:44:02 +0200

    tinymce: moves some toolbar buttons

--- tinymce/profiles.py
+++ tinymce/profiles.py
@@ -5,9 +5,9 @@ DEFAULT = {
     'plugins': 'compat3x advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker '
                'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media '
                'nonbreaking save table contextmenu directionality emoticons template paste textcolor codesample',
-    'toolbar': 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | '
-               'bullist numlist outdent indent | link image | print preview media fullpage | '
-               'forecolor backcolor emoticons',
+    'toolbar': 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | '
+               'bullist numlist outdent indent | link image media emoticons | print preview fullpage | '
+               'forecolor backcolor',
 }
 
 SIMPLE = {
