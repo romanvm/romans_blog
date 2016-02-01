03b8e58b2b0691fc8abb28e32c05846294507a3d
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2016-02-01 13:25:21 +0200

    Adds compat3x plugin to default TinyMCE config

--- tinymce/profiles.py
+++ tinymce/profiles.py
@@ -1,7 +1,11 @@
 DEFAULT = {
     'theme': 'modern',
-    'plugins': 'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking save table contextmenu directionality emoticons template paste textcolor',
-    'toolbar': 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons', 
+    'plugins': 'compat3x advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker '
+               'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media '
+               'nonbreaking save table contextmenu directionality emoticons template paste textcolor codesample',
+    'toolbar': 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | '
+               'bullist numlist outdent indent | link image | print preview media fullpage | '
+               'forecolor backcolor emoticons',
 }
 SIMPLE = {
     'theme': 'modern',
