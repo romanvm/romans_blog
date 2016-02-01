5eac613cc0fccedc2e4db2126e459d17b573caf8
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2016-02-01 17:41:20 +0200

    Minor change to tinymce.profiles

--- tinymce/profiles.py
+++ tinymce/profiles.py
@@ -4,11 +4,20 @@ DEFAULT = {
     'theme': 'modern',
     'plugins': 'compat3x advlist autolink link image lists charmap print preview hr anchor pagebreak '
                'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media '
-               'nonbreaking save table contextmenu directionality emoticons template paste textcolor codesample',
+               'nonbreaking save table contextmenu directionality emoticons template paste textcolor',
     'toolbar': 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | '
                'bullist numlist outdent indent | link image media emoticons | print preview fullpage | '
                'forecolor backcolor',
 }
+
+if settings.USE_CODESAMPLE:
+    DEFAULT['plugins'] += ' codesample'
+    DEFAULT['toolbar'] += ' | codesample'
+if settings.USE_SPELLCHECKER:
+    DEFAULT['plugins'] += ' spellchecker'
+    DEFAULT['toolbar'] += ' | spellchecker'
+    DEFAULT['spellchecker_languages'] = 'English (US)=en_US,English (UK)=en_UK,French=fr_FR,German=de_DE'
+    DEFAULT['spellchecker_language'] = 'en_US'
 
 SIMPLE = {
     'theme': 'modern',
@@ -20,11 +29,3 @@ SIMPLE = {
     'schema': 'html5',
     'statusbar': True,
 }
-
-if settings.USE_CODESAMPLE:
-    DEFAULT['toolbar'] += ' | codesample'
-if settings.USE_SPELLCHECKER:
-    DEFAULT['plugins'] += ' spellchecker'
-    DEFAULT['toolbar'] += ' | spellchecker'
-    DEFAULT['spellchecker_languages'] = 'English (US)=en_US,English (UK)=en_UK,French=fr_FR,German=de_DE'
-    DEFAULT['spellchecker_language'] = 'en_US'
