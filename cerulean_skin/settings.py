3b2705e7cbd9e2437128d6dd95ca635a93dcfadc
Roman Miroshnychenko <romanvm@yandex.ru>
2016-02-07 20:44:24 +0200

    Cerulean skin: add skin styles to TinyMCE as an array, not as a string

--- cerulean_skin/settings.py
+++ cerulean_skin/settings.py
@@ -6,39 +6,36 @@
 Contains the variables defining specific style classes for TinyMCE editor
 """
 
-import os
-from django.conf import settings
-from django.core.files.storage import FileSystemStorage
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.utils.translation import ugettext as _
 
 # Image styles for TinyMCE 4
 IMAGE_CLASS_LIST = [
-    {'title': 'Responsive', 'value': 'img-responsive'},
-    {'title': 'Rounded', 'value': 'img-responsive img-rounded'},
-    {'title': 'Thumbnail', 'value': 'img-responsive img-thumbnail'},
-    {'title': 'Circle', 'value': 'img-responsive img-circle'},
+    {'title': _('Responsive'), 'value': 'img-responsive'},
+    {'title': _('Rounded'), 'value': 'img-responsive img-rounded'},
+    {'title': _('Thumbnail'), 'value': 'img-responsive img-thumbnail'},
+    {'title': _('Circle'), 'value': 'img-responsive img-circle'},
 ]
 # Table styles for TinyMCE 4
 TABLE_CLASS_LIST = [
-    {'title': 'Simple', 'value': 'table'},
-    {'title': 'Bordered', 'value': 'table table-bordered'},
-    {'title': 'Striped', 'value': 'table table-striped'},
-    {'title': 'Condensed', 'value': 'table table-condensed'},
+    {'title': _('Simple'), 'value': 'table'},
+    {'title': _('Bordered'), 'value': 'table table-bordered'},
+    {'title': _('Striped'), 'value': 'table table-striped'},
+    {'title': _('Condensed'), 'value': 'table table-condensed'},
 ]
 # Table row styles for TinyMCE 4
 TABLE_ROW_CLASS_LIST = [
-    {'title': 'None', 'value': ''},
-    {'title': 'Green', 'value': 'success'},
-    {'title': 'Red', 'value': 'danger'},
-    {'title': 'Blue', 'value': 'info'},
+    # Translators: "None" means no styles for a table
+    {'title': _('None'), 'value': ''},
+    {'title': _('Green'), 'value': 'success'},
+    {'title': _('Red'), 'value': 'danger'},
+    {'title': _('Blue'), 'value': 'info'},
 ]
 
-if settings.DEBUG:
-    storage = FileSystemStorage(location=os.path.join(os.path.dirname(__file__), 'static'))
-else:
-    storage = FileSystemStorage(location=settings.STATIC_ROOT)
 # Content styles for TinyMCE 4
-CSS = ('cerulean_skin/css/bootstrap.min.css', 'cerulean_skin/css/cerulean_skin.css')
-content_style = ''
-for css in CSS:
-    with storage.open(css) as file_obj:
-        content_style += file_obj.read().decode('utf-8')
+CONTENT_STYLE = [
+    staticfiles_storage.url('cerulean_skin/css/bootstrap.min.css'),
+    staticfiles_storage.url('cerulean_skin/css/font-awesome.min.css'),
+    staticfiles_storage.url('cerulean_skin/css/cerulean_skin.css'),
+    staticfiles_storage.url('cerulean_skin/css/prism.css'),
+]
