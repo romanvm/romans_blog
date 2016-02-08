fd1232ad81041343217f423d3d1e9c9d27440c6f
Roman Miroshnychenko <romanvm@yandex.ru>
2016-02-08 20:56:32 +0200

    Cerulean skin: removes localization for TinyMCE styles

--- cerulean_skin/settings.py
+++ cerulean_skin/settings.py
@@ -7,33 +7,32 @@ Contains the variables defining specific style classes for TinyMCE editor
 """
 
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.utils.translation import ugettext as _
 
 # Image styles for TinyMCE 4
 IMAGE_CLASS_LIST = [
-    {'title': _('Responsive'), 'value': 'img-responsive'},
-    {'title': _('Rounded'), 'value': 'img-responsive img-rounded'},
-    {'title': _('Thumbnail'), 'value': 'img-responsive img-thumbnail'},
-    {'title': _('Circle'), 'value': 'img-responsive img-circle'},
+    {'title': 'Responsive', 'value': 'img-responsive'},
+    {'title': 'Rounded', 'value': 'img-responsive img-rounded'},
+    {'title': 'Thumbnail', 'value': 'img-responsive img-thumbnail'},
+    {'title': 'Circle', 'value': 'img-responsive img-circle'},
 ]
 # Table styles for TinyMCE 4
 TABLE_CLASS_LIST = [
-    {'title': _('Simple'), 'value': 'table'},
-    {'title': _('Bordered'), 'value': 'table table-bordered'},
-    {'title': _('Striped'), 'value': 'table table-striped'},
-    {'title': _('Condensed'), 'value': 'table table-condensed'},
+    {'title': 'Simple', 'value': 'table'},
+    {'title': 'Bordered', 'value': 'table table-bordered'},
+    {'title': 'Striped', 'value': 'table table-striped'},
+    {'title': 'Condensed', 'value': 'table table-condensed'},
 ]
 # Table row styles for TinyMCE 4
 TABLE_ROW_CLASS_LIST = [
     # Translators: "None" means no styles for a table
-    {'title': _('None'), 'value': ''},
-    {'title': _('Green'), 'value': 'success'},
-    {'title': _('Red'), 'value': 'danger'},
-    {'title': _('Blue'), 'value': 'info'},
+    {'title': 'None', 'value': ''},
+    {'title': 'Green', 'value': 'success'},
+    {'title': 'Red', 'value': 'danger'},
+    {'title': 'Blue', 'value': 'info'},
 ]
 
 # Content styles for TinyMCE 4
-CONTENT_STYLE = [
+CONTENT_CSS = [
     staticfiles_storage.url('cerulean_skin/css/bootstrap.min.css'),
     staticfiles_storage.url('cerulean_skin/css/font-awesome.min.css'),
     staticfiles_storage.url('cerulean_skin/css/cerulean_skin.css'),
