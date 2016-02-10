d1c5773ca8df551f55e0d5942b61d8cf57b85ac1
Roman Miroshnychenko <romanvm@yandex.ru>
2016-02-10 20:10:46 +0200

    Adds publish_docs.py

---
+++ publish_docs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Roman Miroshnychenko aka Roman V.M.
+# E-mail: romanvm@yandex.ua
+"""
+Build and publish Sphinx documentation via Travis CI
+"""
+
+from __future__ import print_function
+import os
+import sys
+
+if os.environ['TRAVIS_BRANCH'] != 'master' and os.environ['TRAVIS_PULL_REQUEST'] != 'false':
+    print('This is not a master branch. Exiting...')
+    sys.exit(0)
+
+basedir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(basedir, 'foo'))
+
+import romans_blog
+
+gh_token = os.environ['GH_TOKEN']
+if not gh_token:
+    raise RuntimeError('GH_TOKEN is not defined!')
+gh_repo = 'https://{gh_token}@github.com/{repo_slug}.git'.format(gh_token=gh_token,
+                                                                 repo_slug=os.environ['TRAVIS_REPO_SLUG'])
+docs = os.path.join(basedir, 'docs')
+html = os.path.join(docs, '_build', 'html')
+os.chdir(docs)
+os.system('make html')
+os.chdir(html)
+os.system('git init')
+os.system('git config user.name "Roman Miroshnychenko"')
+os.system('git config user.email "romanvm@yandex.ua"')
+open('.nojekyll', 'w').close()
+os.system('git add --all .')
+os.system('git commit -m "Updates documentation to v.{0}"'.format(romans_blog.__version__))
+os.system('git push -f -q "{gh_repo}" HEAD:gh-pages'.format(gh_repo=gh_repo))
+print('Documentation published successfully.')
