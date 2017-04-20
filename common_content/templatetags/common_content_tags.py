aa44c6640a8398f37ac5f9776b799414f6d3843f
Roman Miroshnychenko <romanvm@yandex.ru>
2017-04-20 22:23:24 +0300

    Remove unnecessary get_url template tag

--- common_content/templatetags/common_content_tags.py
+++ common_content/templatetags/common_content_tags.py
@@ -4,7 +4,6 @@
 
 from django import template
 from django.conf import settings
-from django.core.urlresolvers import reverse
 
 register = template.Library()
 
@@ -17,19 +16,6 @@ def get_site_name():
     :return: site name from settings
     """
     return settings.SITE_NAME
-
-
-@register.assignment_tag
-def get_url(url_name, **kwargs):
-    """
-    Assignment tag
-
-    Get reverse URL as a template variable
-
-    :param url_name: the name of a path from urls
-    :return: relative URL path
-    """
-    return reverse(url_name, kwargs=kwargs)
 
 
 @register.inclusion_tag('common_content/google_analytics.html', takes_context=False)
