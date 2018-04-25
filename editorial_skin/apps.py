75fc61b5d070a78b330b8252122f8063ec536f52
Roman Miroshnychenko <roman1972@gmail.com>
2018-04-25 22:05:17 +0300

    Remove editorial skin

--- editorial_skin/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class EditorialSkinConfig(AppConfig):
-    name = 'editorial_skin'
-    verbose_name = _('Editorial Skin')
--- editorial_skin/templatetags/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-# Created on: 06.10.2017
-# Author: Roman Miroshnychenko aka Roman V.M. (roman1972@gmail.com)
-
-
