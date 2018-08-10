ebfd0ca639f3185e833053e66df65281f1fbfaf2
Roman Miroshnychenko <roman1972@gmail.com>
2018-08-10 15:32:22 +0300

    Add save on top buttons to SiteConfiguration admin

--- common_content/admin.py
+++ common_content/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 from .models import SiteConfiguration
 
+SingletonModelAdmin.save_on_top = True
 admin.site.register(SiteConfiguration, SingletonModelAdmin)
