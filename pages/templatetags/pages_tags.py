b46872748b1e7e89dcad5761d4796d73c980977f
Roman Miroshnychenko <roman1972@gmail.com>
2018-08-11 10:40:31 +0300

    Implement JSON-LD data for a page

--- pages/templatetags/pages_tags.py
+++ pages/templatetags/pages_tags.py
@@ -3,6 +3,7 @@
 # Created on: 19.11.2015
 # Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
 
+import json
 from django import template
 from ..models import MenuLink
 
@@ -17,3 +18,33 @@ def get_pages_menu_links():
     :return: menu links that have attached pages
     """
     return MenuLink.objects.have_pages()
+
+
+@register.inclusion_tag('common_content/json-ld.html', takes_context=True)
+def page_json_ld(context):
+    """
+    Renders JSON-LD for a page
+
+    :param context: parent template context
+    :type context: dict
+    :return: context for json-ld template
+    :rtype: dict
+    """
+    site_url = '{}://{}'.format(
+        context['request'].scheme,
+        context['request'].get_host()
+    )
+    json_ld = {
+        '@context': 'http://schema.org',
+        '@type': 'WebPage',
+        'name': context['menu_link'].page.title,
+        'url': site_url + context['request'].path,
+        'description': context['menu_link'].page.meta_description,
+        'image': {
+            '@type': 'imageObject',
+            'url': site_url + context['menu_link'].page.featured_image.url,
+            'height': context['menu_link'].page.featured_image.height,
+            'width': context['menu_link'].page.featured_image.width
+        },
+    }
+    return {'json_ld': json.dumps(json_ld, indent=2)}
