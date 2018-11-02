a37d4e6a6d82ba0ee096287c7e2768a7b56470eb
Roman Miroshnychenko <roman1972@gmail.com>
2018-11-02 09:47:09 +0200

    Try to fix tests Travis CI #3

--- romans_blog/settings/dev.py
+++ romans_blog/settings/dev.py
@@ -18,7 +18,7 @@ DATABASES = {
             'HOST': os.getenv('DB_HOST') or 'localhost',
             'OPTIONS': {
                 'charset': 'utf8mb4',
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES';",
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
                 'isolation_level': 'read committed'
             },
             'TEST': {
@@ -30,6 +30,7 @@ DATABASES = {
 
 if os.getenv('CI'):
     DATABASES['default']['OPTIONS']['init_command'] += \
+        ",row_format=DYNAMIC;" \
         "SET GLOBAL default_storage_engine = innodb," \
         "innodb_file_format = Barracuda," \
         "innodb_file_per_table = ON," \
