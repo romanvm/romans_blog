b48832e379126d46d49b26d7adde9a331a94eadf
Roman Miroshnychenko <roman1972@gmail.com>
2018-11-02 09:38:23 +0200

    Try to fix tests Travis CI

--- romans_blog/settings/dev.py
+++ romans_blog/settings/dev.py
@@ -33,5 +33,4 @@ if os.getenv('CI'):
         "SET GLOBAL default_storage_engine = innodb," \
         "innodb_file_format = Barracuda," \
         "innodb_file_per_table = ON," \
-        "innodb_large_prefix = 1," \
-        "innodb_default_row_format = dynamic"
+        "innodb_large_prefix = 1,"
