1a3a5be110a7da4d2987fcf01c268a2c0c9a13f7
Roman Miroshnychenko <roman1972@gmail.com>
2018-11-11 21:29:26 +0200

    Fix collectstatic

--- romans_blog/settings/__init__.py
+++ romans_blog/settings/__init__.py
@@ -2,7 +2,14 @@
 # License: GPL v.3
 
 # Necessary for imports inside base.py to work properly
+import os
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
 SECRET_KEY = 'dummy'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 from .dev import *
