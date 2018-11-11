c8d63f4ed6048e71a624ec7f4fec18d00187edb1
Roman Miroshnychenko <roman1972@gmail.com>
2018-11-11 21:25:52 +0200

    Commend debug-toolbar in dev settings

--- romans_blog/settings/dev.py
+++ romans_blog/settings/dev.py
@@ -3,9 +3,9 @@
 
 from .base import *
 
-INSTALLED_APPS.insert(-8, 'debug_toolbar')
+# INSTALLED_APPS.insert(-8, 'debug_toolbar')
 
-MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+# MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 INTERNAL_IPS = ['127.0.0.1']
 
