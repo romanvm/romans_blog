5f612c49bff6c2a7df48cfa2489544788123c5f0
Roman <roman1972@gmail.com>
2021-10-29 03:06:27 +0300

    Use the new virtual environment

--- update_index.sh
+++ update_index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Update search index on pythonanywhere.com
 
-source /home/romanvm/venv35/bin/activate
+source /home/romanvm/venv36/bin/activate
 cd /home/romanvm/romans_blog
 python manage.py update_index
