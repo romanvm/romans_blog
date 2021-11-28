57d114598696fb333fc8c3640434626c6e805b69
Roman <roman1972@gmail.com>
2021-11-29 00:01:28 +0200

    Install project requirements in update-project.sh

--- update-project.sh
+++ update-project.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-. /home/romanvm/venv39/bin/activate
-
 cd /home/romanvm/romans_blog
 
 git fetch
 git pull
+
+. /home/romanvm/venv39/bin/activate
+
+pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
